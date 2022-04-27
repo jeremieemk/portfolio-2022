cdf0ed1af5c25b5247ed6484d4caf61b5b98c12d
jeremie <jeremieemk@gmail.com>
2022-04-27 21:02:06 +0200

    deploy bug fix

--- pages/components/Project.tsx
+++ pages/components/Project.tsx
@@ -1,36 +1,37 @@
 type ProjectProps = {
-    project: {
-        title: string,
-        url: string,
-        description: string,
-        poster: string,
-        videoMp4: string,
-        tech: string[]
-    }
-}
+	project: {
+		title: string;
+		url: string;
+		description: string;
+		poster: string;
+		videoMp4: string;
+		tech: string[];
+	};
+};
 
-
-const Project = ({ project}: ProjectProps) => {
-    return (
-        <div>
-            <div>⌘</div>
-            <a href={project.url} target="_blank" rel="noreferrer">
-                <video autoPlay loop muted poster={project.poster}>
-                  <source src={project.videoMp4} type="video/mp4" />
-                </video>
-              </a>
-              <div>
-              <a href={project.url} target="_blank" rel="noreferrer">
-                  {project.title}
-                </a>
-                <div>
-                  {project.tech.map((stack) => (
-                    <button key={stack}>{stack}</button>
-                  ))}
-                </div>
-              </div>
-        </div>
-      )
-}
+const Project = ({ project }: ProjectProps) => {
+	return (
+		project && (
+			<div>
+				<div>⌘</div>
+				<a href={project.url} target="_blank" rel="noreferrer">
+					<video autoPlay loop muted poster={project.poster}>
+						<source src={project.videoMp4} type="video/mp4" />
+					</video>
+				</a>
+				<div>
+					<a href={project.url} target="_blank" rel="noreferrer">
+						{project.title}
+					</a>
+					<div>
+						{project.tech.map((stack) => (
+							<button key={stack}>{stack}</button>
+						))}
+					</div>
+				</div>
+			</div>
+		)
+	);
+};
 
 export default Project;
