b82fc384b4b523e06f70a8254f3d0d268e762a2a
jeremie <jeremieemk@gmail.com>
2022-04-28 20:30:06 +0200

    added more project data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,20 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import ProjectList from './components/ProjectList'
+import type { NextPage } from "next";
+import Head from "next/head";
+import ProjectList from "./components/ProjectList";
 
 const Home: NextPage = () => {
-  return (
-    <div>
-      <Head>
-        <title>JérémieCodes</title>
-        <meta name="JérémieCodes" content="JérémieCodes portfolio" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <h1 className='text-4xl font-poppins'>Hello World</h1>
-      <div className=' font-poppins text-textColor bg-backgroundColor  '>
-      <ProjectList />
-      </div>
-      
-    </div>
-  )
-}
+	return (
+		<div>
+			<Head>
+				<title>JérémieCodes</title>
+				<meta name="JérémieCodes" content="JérémieCodes portfolio" />
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<div>
+				<ProjectList />
+			</div>
+		</div>
+	);
+};
 
-export default Home
+export default Home;
