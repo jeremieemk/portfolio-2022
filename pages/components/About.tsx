2f5cafd124aa8d4a86c9d4a44ab58bfefd188c60
jeremie <jeremieemk@gmail.com>
2022-05-02 09:45:19 +0200

    added selected projects title

--- pages/components/About.tsx
+++ pages/components/About.tsx
@@ -2,7 +2,7 @@ const About = () => {
 	return (
 		<>
 			<div className="   ">
-				Hello 👋, {`I'm`} <span className=" text-white underline font-bold ">Jérémie</span>. A Javascript Engineer with a
+				Hello 👋, {`I'm`} <span className=" text-white underline font-bold ">Jérémie</span>. A Javascript engineer with a
 				passion for creating fascinating things that live in the digital world.
 			</div>
 			<div>
@@ -14,7 +14,7 @@ const About = () => {
                 <span className=" text-white font-bold transition duration-300 ease-in-out hover:text-green " > Tailwind</span>.
 			</div>
 			<div>
-				When I’m not coding I like to spend my time <a href="https://www.izem.me/work" target="_blank" rel="noreferrer" > <span className="text-white font-bold transform transition duration-300 ease-in-out hover:translate-y-0.5">writing music</span></a>, exploring the world and learning (human) languages.
+				When I’m not coding, I like to spend my time <a href="https://www.izem.me/work" target="_blank" rel="noreferrer" > <span className="text-white font-bold transform transition duration-300 ease-in-out hover:translate-y-0.5">writing music</span></a>, exploring the world and learning (human) languages.
 			</div>
 		</>
 	);
--- pages/components/ProjectList.tsx
+++ pages/components/ProjectList.tsx
@@ -3,11 +3,17 @@ import Project from "./Project";
 
 const ProjectList = () => {
 	return (
-		<div className=" grid grid-cols-1 gap-8 md:grid-cols-2 md:gap-16  ">
-			{projectData && projectData.map((project) => {
-				return <Project key={project.title} project={project} />;
-			})}
-		</div>
+		<section>
+			<h2 className=" text-2xl md:text-5xl font-bold text-white px-1 py-4 rounded-lg bg-darkGrey2 w-fit mb-6"  >SELECTED PROJECTS</h2>
+			<div className=" grid grid-cols-1 gap-8 md:grid-cols-2 md:gap-16  ">
+				{projectData && projectData.map((project) => {
+					return <Project key={project.title} project={project} />;
+				})}
+			</div>
+
+		</section>
+
+		
 	);
 };
 
