dd12b2bfd8d07860ea043679bdd016a1dd4f61bb
jeremie <jeremieemk@gmail.com>
2022-05-02 15:12:28 +0200

    deploy bug fix

--- pages/components/Footer.tsx
+++ pages/components/Footer.tsx
@@ -1,13 +1,30 @@
-import { TwitterLogo, Code } from "phosphor-react";
+import { TwitterLogo, Code, EnvelopeOpen, GithubLogo } from "phosphor-react";
 import FooterLink from "./FooterLink";
 
 const Footer = () => {
 	return (
 		<section className="flex justify-between">
 			<div className=" flex w-full gap-4 ">
-				<FooterLink label="TWITTER" color="#007ACC" Icon={TwitterLogo} link="https://twitter.com/iZem" />
+				<FooterLink
+					label="TWITTER"
+					color="#007ACC"
+					Icon={TwitterLogo}
+					link="https://twitter.com/iZem"
+				/>
+				<FooterLink label="EMAIL" color="#f7df1e" Icon={EnvelopeOpen} />
+				<FooterLink
+					label="GITHUB"
+					color="#699E5B"
+					Icon={GithubLogo}
+					link="https://github.com/jeremieemk"
+				/>
 			</div>
-            <FooterLink label="VIEW CODE" color="#DC5EB7" Icon={Code} link="https://github.com/jeremieemk/portfolio-2022" />
+			<FooterLink
+				label="VIEW CODE"
+				color="#DC5EB7"
+				Icon={Code}
+				link="https://github.com/jeremieemk/portfolio-2022"
+			/>
 		</section>
 	);
 };
--- pages/components/FooterLink.tsx
+++ pages/components/FooterLink.tsx
@@ -1,21 +1,27 @@
-import { TwitterLogo } from "phosphor-react";
-
 type FooterLinkProps = {
-    label: string,
-    color: string,
-    Icon : any,
-    link: string
+	label: string;
+	color: string;
+	Icon: any;
+	link?: string;
 };
 
-
 const FooterLink = ({ label, Icon, color, link }: FooterLinkProps) => {
-	return (
-		<a href={link} target="_blank" rel="noreferrer" className=" text-2xl md:text-5xls text-white p-4 rounded-lg bg-darkGrey2 w-fit">
-			<div className="flex gap-2 whitespace-nowrap items-center">
-				<Icon color={color} /> {label}
-			</div>
-		</a>
-	);
+	if (label && Icon && color) {
+		return (
+			<a
+				href={link && link}
+				target="_blank"
+				rel="noreferrer"
+				className=" text-2xl md:text-5xls text-white p-4 rounded-lg bg-darkGrey2 w-fit"
+			>
+				<div className="flex gap-2 whitespace-nowrap items-center">
+					<Icon color={color && color} /> {label && label}
+				</div>
+			</a>
+		);
+	} else {
+		return null
+	}
 };
 
 export default FooterLink;
