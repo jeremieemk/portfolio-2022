26149d4a565eaf91ac43b7e6d9eaf1b932e72333
jeremie <jeremieemk@gmail.com>
2022-05-03 16:35:50 +0200

    improved animations

--- pages/components/ClipboardModal.tsx
+++ pages/components/ClipboardModal.tsx
@@ -1,17 +1,20 @@
+import { motion } from "framer-motion";
+
 type ClipboardModalProps = {
 	showModal: boolean;
 };
 
 const ClipboardModal = ({ showModal }: ClipboardModalProps) => {
-	return (
-		showModal ? (
-			<div
-				className="transition duration-300 ease-in-out text-base md:text-lg p-3 rounded-lg bg-darkGrey2 w-fit h-fit"
-			>
-				jeremieemk@gmail.com was copied to your clipboard
-			</div>
-		) : null
-	);
+	return showModal ? (
+		<motion.div
+			animate={{ opacity: 1 }}
+			transition={{ duration: 0.2 }}
+			initial={{ opacity: 0 }}
+			className="text-base md:text-lg p-3 rounded-lg bg-darkGrey2 w-fit h-fit"
+		>
+			jeremieemk@gmail.com was copied to your clipboard
+		</motion.div>
+	) : null;
 };
 
 export default ClipboardModal;
