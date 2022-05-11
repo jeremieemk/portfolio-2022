5d5945653f346c66f3defc4402429cd9b00c9029
jeremie <jeremieemk@gmail.com>
2022-05-11 14:26:50 +0200

    added avatar + layout adjust

--- pages/components/About.tsx
+++ pages/components/About.tsx
@@ -1,21 +1,24 @@
 import { motion } from "framer-motion";
-
+import Image from "next/image";
+ 
 const About = () => {
 	return (
-		<div className="flex flex-col gap-6 md:gap-10 ">
-			<p>
+		<div className="flex flex-col gap-6 md:gap-10  ">
+			<p className="">
 				Hello 👋, {`I'm`}{" "}
-				<span className=" text-white underline font-bold ">Jérémie</span>. A
-				Javascript engineer with a passion for creating fascinating things that
-				live in the digital world.
+				<span className=" text-white underline font-bold relative cursor-pointer dev-name ">
+					Jérémie <img src="/avatar.png" alt="avatar" className=" avatar"  />{" "}
+				</span>
+				. A Front-end engineer with a passion for creating fascinating things
+				that live in the digital world.
 			</p>
 			<p>
-				I mainly work within the{" "}
+				Always learning new tools & technologies, I am committed to the
+				philosophy of life-long learning. I have experience with{" "}
 				<span className=" text-white font-bold  transition-all duration-300 ease-in-out hover:text-pink">
 					React{" "}
-				</span>{" "}
-				ecosystem, but {`I'm`} always learning new tools & technologies. On the
-				Frontend I have experience with
+				</span>
+				,
 				<span className=" text-white font-bold transition duration-300 ease-in-out hover:text-yellow ">
 					{" "}
 					TypeScript
@@ -53,7 +56,8 @@ const About = () => {
 						writing music
 					</span>
 				</motion.a>
-				, exploring the world and learning (human) languages.
+				, exploring the world and learning (human) languages. I am currently
+				based in Europe.
 			</p>
 		</div>
 	);
